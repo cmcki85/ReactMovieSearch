304f0a642a66d6853fc63e500aa058d1198b53d2
Cameron <cameronsmowing@gmail.com>
2019-11-18 12:06:34 +1000

    React Fluff Removed, App Dev Started.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Fluff Removed!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
